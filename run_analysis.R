b6ea845377ba327c1bdf7ccbea669222a60a46a9
zuex0008 <zuex0008@hotmail.com>
2016-02-10 17:08:40 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,24 +2,24 @@
 library(dplyr)
 
 #colect feature text for column names
-dtfeature <- read.table("./UCI HAR Dataset/features.txt",header = FALSE)
+dtfeature = read.table("./UCI HAR Dataset/features.txt",header = FALSE)
 
 # collect test data from folder
-dttest <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+dttest = read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
 # rename column with the names extracted from feature.txt
 colnames(dttest) = as.character(dtfeature[,2]) 
-dttestlbl <- read.table("./UCI HAR Dataset/test/Y_test.txt",header = FALSE,col.names = ("activity"))
-dttestsubject <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE,col.names = ("subject"))
+dttestlbl = read.table("./UCI HAR Dataset/test/Y_test.txt",header = FALSE,col.names = ("activity"))
+dttestsubject = read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE,col.names = ("subject"))
 
 # collect train data from folder
-dttrain <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+dttrain = read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
 # rename column with the names extracted from feature.txt
 colnames(dttrain) =  as.character(dtfeature[,2])
-dttrainlbl <- read.table("./UCI HAR Dataset/train/Y_train.txt",header = FALSE,col.names = ("activity"))
-dttrainsubject <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE,col.names = ("subject"))
+dttrainlbl = read.table("./UCI HAR Dataset/train/Y_train.txt",header = FALSE,col.names = ("activity"))
+dttrainsubject = read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE,col.names = ("subject"))
 
 # collecct trainig pattern 
-dtactivity <-  read.table("./UCI HAR Dataset/activity_labels.txt",header = FALSE, col.names = c("activitynumber","activitynames"))
+dtactivity = read.table("./UCI HAR Dataset/activity_labels.txt",header = FALSE, col.names = c("activitynumber","activitynames"))
 
 # merging test data with activitynames and subjects to create a new table 
 dt1test = cbind(dttestsubject,dttestlbl,dttest)
@@ -37,14 +37,14 @@ dt3 = dt2
 dt3$activity = as.character(dtactivity[dt3$activity,2])
 
 # Ans4 : label varible names with decsriptive names  
-names(dt4)<-gsub("std()", "SD", names(dt4))
-names(dt4)<-gsub("mean()", "MEAN", names(dt4))
-names(dt4)<-gsub("^t", "time", names(dt4))
-names(dt4)<-gsub("^f", "frequency", names(dt4))
-names(dt4)<-gsub("Acc", "Accelerometer", names(dt4))
-names(dt4)<-gsub("Gyro", "Gyroscope", names(dt4))
-names(dt4)<-gsub("Mag", "Magnitude", names(dt4))
-names(dt4)<-gsub("BodyBody", "Body", names(dt4))
+names(dt4) = gsub("std()", "SD", names(dt4))
+names(dt4) = gsub("mean()", "MEAN", names(dt4))
+names(dt4) = gsub("^t", "time", names(dt4))
+names(dt4) = gsub("^f", "frequency", names(dt4))
+names(dt4) = gsub("Acc", "Accelerometer", names(dt4))
+names(dt4) = gsub("Gyro", "Gyroscope", names(dt4))
+names(dt4) = gsub("Mag", "Magnitude", names(dt4))
+names(dt4) = gsub("BodyBody", "Body", names(dt4))
 
 # Ans5 : create tidy data set with the average of earch activity and subject 
 dt4 = dt3 %>% group_by(activity,subject) %>% summarise_each(funs(mean)) 
